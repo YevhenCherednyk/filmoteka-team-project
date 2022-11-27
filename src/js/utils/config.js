4ca7ce5c505b077abf7ed8ece9fa6c952397be99
Yevhen Cherednyk <yevgen.cherednyk@gmail.com>
2022-11-27 13:10:47 +0200

    change close icon, add line-height: in the footer.scss, add some configs in configs.js, parcial requested an update and I did

---
+++ src/js/utils/config.js
@@ -0,0 +1,11 @@
+const urls = {
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  TRENDINGS_MOVIES_PATH_URL: 'trending/movie/day',
+  SEARCH_MOVIE_PATH_URL: 'search/movie',
+  MOVIE_DETAILS_PATH_URL: 'movie/',
+  MOVIE_GENRES_PATH_URL: 'genre/movie/list',
+};
+
+const API_KEY = '19011014b9b53c4fd496d37c25f2b619';
+
+export { urls, API_KEY };
