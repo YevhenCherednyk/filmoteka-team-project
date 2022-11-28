805778495abd98a888ffd987f659f478d9fa1ce6
YevhenCherednyk <yevgen.cherednyk@gmail.com>
2022-11-28 20:10:31 +0000

    Deploying to gh-pages from @ YevhenCherednyk/filmoteka-team-project@af6e6f84a9fff150e277947b51e258843dc19a46 🚀

---
+++ library.3af5eb81.js
@@ -0,0 +1,2 @@
+!function(){var e={modalCloseBtn:document.querySelector("[data-modal-close]"),modalOpenBtn:document.querySelector("#open-modal-team"),backdrop:document.querySelector("#team-modal")};function o(){document.body.classList.toggle("modal-is-open"),e.backdrop.classList.toggle("is-hidden")}e.modalOpenBtn.addEventListener("click",o),e.modalCloseBtn.addEventListener("click",o)}();
+//# sourceMappingURL=library.3af5eb81.js.map
