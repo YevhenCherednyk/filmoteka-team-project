8a6a8a5c8d7b63fe216dc5551d5ef53139c205b8
GolovinD <golovin.dmytro@gmail.com>
2022-11-29 00:12:51 +0200

    js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 import './js/api-service/trending';
+import './js/api-service/trendingAndGenres';
+import './js/api-service/searchMovie';
+import './js/api-service/searcheAndRender';
 
+import './js/pagination/paginationManager';
+import './js/modal-film-card/movie-details';
 
-
+import './js/team-modal/team-modal.js';
