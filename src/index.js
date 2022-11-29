e0b505e2b7b141f9612a3fc08ca6dad2e6438ffd
Yevhen Cherednyk <yevgen.cherednyk@gmail.com>
2022-11-29 12:19:37 +0200

    add paginationEventHandler.js in index.js and instaled notifix

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import './js/api-service/trendingAndGenres';
 import './js/api-service/searchMovie';
 import './js/api-service/searcheAndRender';
 
-import './js/pagination/paginationManager';
+import './js/pagination/paginationEventHandler';
 import './js/modal-film-card/movie-details';
 
 import './js/team-modal/team-modal.js';
