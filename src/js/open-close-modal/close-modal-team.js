d05dd3ba3aed4bde94c85e847541b41786631942
IngaPirogova <ingeborga.pirogova@gmail.com>
2022-11-29 19:00:40 +0300

    Update close-modal-team.js

--- src/js/open-close-modal/close-modal-team.js
+++ src/js/open-close-modal/close-modal-team.js
@@ -1,4 +1,11 @@
-window.addEventListener('keydown', onEscKeyPress);
+function toggleModal() {
+    document.body.classList.toggle("modal-is-open")
+    refs.backdrop.classList.toggle("is-hidden");
+    window.addEventListener('keydown', onEscKeyPress); 
+}
+
+
+// window.addEventListener('keydown', onEscKeyPress); 
 refs.backdrop.addEventListener('click', onBackdropClick);
  
 
