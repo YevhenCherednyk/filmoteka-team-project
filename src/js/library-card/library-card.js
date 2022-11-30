68720a13b852ce5eb376f11ff6edc0ed9b9caa37
Valentyn Herasymenko <valikgera2002@gmail.com>
2022-11-30 18:13:58 +0200

    made function

--- src/js/library-card/library-card.js
+++ src/js/library-card/library-card.js
@@ -1,43 +1,57 @@
-import findGenres from '../all-genres/find-genres';
 import dateOptimizer from '../api-service/date-optimizer';
 import titleOptimizer from '../api-service/title-optimizer';
-import picturePathPlace from '../api-service/placeholder';
 
-export default function libraryCardMarkup(data, genres) {
-  const card = data
-    .map(
-      ({
-        id,
-        title,
-        name,
-        genre_ids,
-        vote_average,
-        release_date,
-        first_air_date,
-        poster_path,
-      }) => {
-        return `
-        <li class="films-list__item" data-id="${id}">
-  <img src="${picturePathPlace(poster_path)}" alt="${titleOptimizer(
-          title,
-          name
-        )}" />
-  <h2 class="films-list__title">${titleOptimizer(title, name)}</h2>
-  <span class="films-list__info films-list__ganre">${findGenres(
-    genres,
-    genre_ids
-  )}</span> <span class="films-list__info">&#10072;</span> <span
-    class="films-list__info release-date">${dateOptimizer(
-      release_date,
-      first_air_date
-    )}</span><span class="films-list__info films-rating">${vote_average.toFixed(
-          1
-        )}</span>
-  </div>
-</li>
-        `;
-      }
-    )
-    .join('');
-  return card;
+const refs = {
+  libraryUl: document.querySelector("#library")
+}
+
+const libraryMovies = localStorage.getItem('watched');
+console.log(libraryMovies);
+
+function createMovieLibraryMarkup() {
+  const watched = JSON.parse(libraryMovies);
+  
+  if (!watched || !watched.length) {
+    return
+  } 
+
+  return watched.map(({id, poster_path, title, name, vote_average, genres, release_date, first_air_date}) =>{ 
+  // console.log(poster_path, title, vote_average, vote_count, popularity, original_title, overview)
+  return `<li class="films-list__item" data-id="${id}">
+  <img class="films-list__img" src="https://image.tmdb.org/t/p/w500${poster_path}" alt="${titleOptimizer(
+title,
+name
+)}" />
+  <h2 class="films-list__title">${titleOptimizer(
+    title,
+    name
+    )}</h2>
+  <span class="films-list__info films-list__ganre">${genresList(genres)}</span> <span class="films-list__info">&#10072;</span> <span class="films-list__info release-date">${dateOptimizer(
+release_date,
+first_air_date
+)}
+<span class="films-list__info films-rating "> ${vote_average.toFixed(1)}</span>
+</li>`
+  }).join("");
+}
+
+function renderMovieDetailsMarkup(data) {
+  refs.libraryUl.innerHTML = data;
+}
+
+renderMovieDetailsMarkup(createMovieLibraryMarkup(watched));
+
+function genresList (genres) {
+const genreList = genres.map(({name}) =>  
+  name)
+
+  if (genreList.length === 0) {
+    genreList.push('Other');
+  }
+
+  if (genreList.length > 2) {
+    return [genreList[0], genreList[1], 'Other'].join(', ');
+  }
+
+  return genreList.join(', ');
 }
