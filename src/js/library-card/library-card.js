7a322f666dca4cda58d9109047e86615ef0f0bfe
Valentyn Herasymenko <valikgera2002@gmail.com>
2022-12-01 02:13:01 +0200

    made localsotrage

--- src/js/library-card/library-card.js
+++ src/js/library-card/library-card.js
@@ -2,31 +2,51 @@ import dateOptimizer from '../api-service/date-optimizer';
 import titleOptimizer from '../api-service/title-optimizer';
 
 const refs = {
-  libraryUl: document.querySelector("#library")
+  libraryUl: document.querySelector("#library"),
+  BtnWatched: document.querySelector("#watched"),
+  BtnQueue: document.querySelector("#queue"),
 }
 
 const libraryMovies = localStorage.getItem('watched');
-console.log(libraryMovies);
+
+
+const queueMovies = localStorage.getItem('queue');
+
+refs.BtnWatched.addEventListener("click", () => {
+  renderMovieDetailsMarkup(createMovieLibraryMarkup(libraryMovies))
+});
+
+refs.BtnQueue.addEventListener("click", () => {
+  refs.libraryUl.innerHTML = "";
+  renderMovieDetailsMarkup(createMovieQueueMarkup(queueMovies))
+});
+
+
+
+function logClick(event) {
+  console.log(event)
+}
 
 function createMovieLibraryMarkup() {
   const watched = JSON.parse(libraryMovies);
+  console.log(watched);
   
   if (!watched || !watched.length) {
     return
   } 
 
-  return watched.map(({id, poster_path, title, name, vote_average, genres, release_date, first_air_date}) =>{ 
-  // console.log(poster_path, title, vote_average, vote_count, popularity, original_title, overview)
+  return watched.map(({id, poster_path,title,name,vote_average,original_title,genres,overview, release_date, first_air_date}) => { 
   return `<li class="films-list__item" data-id="${id}">
   <img class="films-list__img" src="https://image.tmdb.org/t/p/w500${poster_path}" alt="${titleOptimizer(
 title,
 name
 )}" />
   <h2 class="films-list__title">${titleOptimizer(
-    title,
+    original_title,
     name
     )}</h2>
-  <span class="films-list__info films-list__ganre">${genresList(genres)}</span> <span class="films-list__info">&#10072;</span> <span class="films-list__info release-date">${dateOptimizer(
+    <span class="films-list__info films-list__ganre">${genresList(genres)}
+  </span> <span class="films-list__info">&#10072;</span> <span class="films-list__info release-date">${dateOptimizer(
 release_date,
 first_air_date
 )}
@@ -35,23 +55,56 @@ first_air_date
   }).join("");
 }
 
+function createMovieQueueMarkup() {
+  const queue = JSON.parse(queueMovies);
+  console.log(watched);
+  
+  if (!queue || !queue.length) {
+    return
+  } 
+
+  return queue.map(({id, poster_path,title,name,vote_average,original_title,genres,overview, release_date, first_air_date}) => { 
+  return `<li class="films-list__item" data-id="${id}">
+  <img class="films-list__img" src="https://image.tmdb.org/t/p/w500${poster_path}" alt="${titleOptimizer(
+title,
+name
+)}" />
+  <h2 class="films-list__title">${titleOptimizer(
+    original_title,
+    name
+    )}</h2>
+    <span class="films-list__info films-list__ganre">${genresList(genres)}
+  </span> <span class="films-list__info">&#10072;</span> <span class="films-list__info release-date">${dateOptimizer(
+release_date,
+first_air_date
+)}
+<span class="films-list__info films-rating "> ${vote_average.toFixed(1)}</span>
+</li>`
+  }).join("");
+}
+
+function genresList (genres) {
+  const genreList =  genres.map(({name}) => name) 
+
+    if (genreList.length === 0) {
+      genreList.push('Other');
+    }
+  
+   
+  if (genreList.length > 2) {
+      return [genreList[0], genreList[1], 'Other'].join(', ');
+    }
+  
+    return genreList.join(', '); 
+  }
+
+console.log(createMovieQueueMarkup(queue));
+
+
 function renderMovieDetailsMarkup(data) {
   refs.libraryUl.innerHTML = data;
 }
 
-renderMovieDetailsMarkup(createMovieLibraryMarkup(watched));
+renderMovieDetailsMarkup(createMovieLibraryMarkup(libraryMovies));
 
-function genresList (genres) {
-const genreList = genres.map(({name}) =>  
-  name)
 
-  if (genreList.length === 0) {
-    genreList.push('Other');
-  }
-
-  if (genreList.length > 2) {
-    return [genreList[0], genreList[1], 'Other'].join(', ');
-  }
-
-  return genreList.join(', ');
-}
--- src/library.js
+++ src/library.js
@@ -1,3 +1,5 @@
 import './js/team-modal/team-modal.js';
 
+import './js/library-card/library-card';
+
 import './js/scrol-to-top/scrollToTop';
