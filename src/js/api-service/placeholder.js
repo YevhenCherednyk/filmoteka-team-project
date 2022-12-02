16c64c5141d4ce4474f1f1fef39b47955863f116
Yevhen Cherednyk <yevgen.cherednyk@gmail.com>
2022-12-02 20:37:52 +0200

    change library card

--- src/js/api-service/placeholder.js
+++ src/js/api-service/placeholder.js
@@ -1,6 +1,6 @@
 export default function picturePathPlace(path) {
   if (path == null) {
-    return 'https://via.placeholder.com/800x1200.svg/000000/ffffff?text=Sorry+,+image+loading+is+failed';
+    return 'https://img.freepik.com/free-vector/coming-soon-display-background-with-focus-light_1017-33741.jpg';
   }
 
   return `https://image.tmdb.org/t/p/w500${path}`;
