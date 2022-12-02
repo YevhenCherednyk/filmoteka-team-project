5080d77af3878681b483254f34462c8a36e4ee11
Yevhen Cherednyk <yevgen.cherednyk@gmail.com>
2022-12-02 17:53:08 +0200

    fixed bug with library

--- src/library.js
+++ src/library.js
@@ -4,6 +4,5 @@ import './js/library-card/library-card';
 
 import './js/scrol-to-top/scrollToTop';
 
-import './js/pagination/paginationEventHandler';
 import './js/modal-film-card/movie-details';
 import './js/modal-film-card/add-to-localStorage';
