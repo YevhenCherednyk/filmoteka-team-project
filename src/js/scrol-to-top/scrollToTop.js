0fdef977ded9f42108ea3c75a60209d9362270d9
AnatoliiStepanovLikhoi <105781107+AnatoliiStepanovLikhoi@users.noreply.github.com>
2022-12-04 10:48:16 +0200

    added team photos & throttle to toTopBtn

--- src/js/scrol-to-top/scrollToTop.js
+++ src/js/scrol-to-top/scrollToTop.js
@@ -1,4 +1,4 @@
-// import throttle from "lodash.throttle";
+import throttle from "lodash.throttle";
 
 const scrollBtn = document.querySelector('.to-top');
 const filmListRef = document.querySelector('.films-list');
@@ -13,15 +13,14 @@ if (scrollBtn)
 
 filmListRef.addEventListener('click', hideToTopBtn);
 
-// document.addEventListener('scroll', throttle(btnVisibility, 500));
-
-document.addEventListener('scroll', btnVisibility);
+document.addEventListener('scroll', throttle(btnVisibility, 250));
 
 function hideToTopBtn() {
   scrollBtn.classList.add('is-hidden');
 }
 
 function btnVisibility() {
+
   window.scrollY > 150
     ? scrollBtn.classList.remove('is-hidden')
     : scrollBtn.classList.add('is-hidden');
